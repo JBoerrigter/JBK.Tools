d38b440fff9dec452f7f242c4ef4d5df139f10ac
Jascha Boerrigter <Jascha.Boerrigter@hotmail.com>
2025-07-20 17:03:34 +0200

    Updated to dotnet 9

--- JBK.Tools.ModelConverter/Program.cs
+++ JBK.Tools.ModelConverter/Program.cs
@@ -1,41 +1,53 @@
 ﻿
 using JBK.Tools.ModelConverter;
 using JBK.Tools.ModelConverter.Exporter;
-using System.Diagnostics;
+
+
+Console.WriteLine("Choose the directory with the .gb files you want to convert:");
+string? directory = Console.ReadLine();
 
 Console.WriteLine("What type of export do you want?");
 Console.WriteLine("1: .obj");
 Console.WriteLine("0. None");
 string? choice = Console.ReadLine();
 
-string[] files = args;
-
-if (Debugger.IsAttached)
-{
-    files = new string[] { /* test file here ( debugging ) */ };
-}
-
 if (choice == "1")
 {
+    DirectoryInfo dirInfo = new(directory);
+    List<string> files = dirInfo.GetFiles()
+        .Where(file => file.Extension.Equals(".gb", StringComparison.OrdinalIgnoreCase))
+        .Select(file => file.FullName)
+        .ToList();
+
+    DirectoryInfo outputDir = new DirectoryInfo(Environment.CurrentDirectory);
+    outputDir = outputDir.CreateSubdirectory("ConvertedModels");
+
     foreach (var fileName in files)
     {
-        using FileStream stream = new(fileName, FileMode.Open);
-        using BinaryReader reader = new(stream);
-
-        GeoBinary gb = new(reader);
-
-        if (gb is null)
+        try
         {
-            Console.WriteLine("Can't convert {0}!", fileName);
-            continue;
+            using FileStream stream = new(fileName, FileMode.Open);
+            using BinaryReader reader = new(stream);
+
+            GeoBinary gb = new(reader);
+
+            if (gb is null)
+            {
+                Console.WriteLine("Can't convert {0}!", fileName);
+                continue;
+            }
+
+            string outputFile = Path.GetFileNameWithoutExtension(fileName);
+
+            for (int i = 0; i < gb.Meshes.Count; i++)
+            {
+                outputFile = Path.Combine(outputDir.FullName, outputFile + "_" + i.ToString() + ".obj");
+                OBJExporter.ExportToObj(outputFile, gb.Meshes[i].Vertices, gb.Meshes[i].UVs, gb.Meshes[i].Normals, gb.Meshes[i].Faces);
+            }
         }
-
-        string outputFile = Path.GetFileNameWithoutExtension(fileName);
-
-        for (int i = 0; i < gb.Meshes.Count; i++)
+        catch (Exception ex)
         {
-            outputFile = Path.Combine(Environment.CurrentDirectory, outputFile + "_" + i.ToString() + ".obj");
-            OBJExporter.ExportToObj(outputFile, gb.Meshes[i].Vertices, gb.Meshes[i].UVs, gb.Meshes[i].Normals, gb.Meshes[i].Faces);
+            Console.WriteLine("{0}: {1}", fileName, ex.Message);
         }
     }
 }
