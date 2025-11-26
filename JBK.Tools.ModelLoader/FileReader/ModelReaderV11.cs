2cdd094df86f1f22186761ac637611a4ce747549
Jascha Boerrigter <Jascha.boerrigter@hotmail.com>
2025-11-26 21:06:43 +0100

    Added support for v11 files

---
+++ JBK.Tools.ModelLoader/FileReader/ModelReaderV11.cs
@@ -0,0 +1,23 @@
+﻿using JBK.Tools.ModelLoader.GbFormat;
+using JBK.Tools.ModelLoader.Parsers;
+
+namespace JBK.Tools.ModelLoader.FileReader;
+
+internal class ModelReaderV11 : IModelFormatReader
+{
+    private readonly BinaryReader _BinaryReader;
+    public ModelReaderV11(BinaryReader binaryReader) => _BinaryReader = binaryReader;
+
+    public NormalizedHeader ReadHeader()
+    {
+        var header = HeaderV11.ReadFrom(_BinaryReader);
+        var normalized = header.ToNormalized();
+        return normalized;
+    }
+
+    public Model ReadModel()
+    {
+        var header = ReadHeader();
+        return SharedModelParser.Parse(header, _BinaryReader);
+    }
+}
